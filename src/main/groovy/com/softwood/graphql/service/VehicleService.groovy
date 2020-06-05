4d59afe2f4873036f906ee55e8c4db8357576c5b
woodmawa <will.woodman@btinternet.com>
2020-06-05 18:21:33 +0100

    added dev tools

--- src/main/groovy/com/softwood/graphql/service/VehicleService.groovy
+++ src/main/groovy/com/softwood/graphql/service/VehicleService.groovy
@@ -5,9 +5,10 @@ import com.softwood.graphql.dao.repository.VehicleRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.List
+import java.util.Optional
+import java.util.stream.Collectors
+
 @Service
 public class VehicleService {
     private final VehicleRepository vehicleRepository
